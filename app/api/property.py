9edbff66cb2c9d3fbd52ff76eb27cccc22fb3429
Filip Polsakiewicz <filip@polsakiewicz.net>
2025-06-13 22:44:14 +0200

    ADD: Event bus

--- app/api/property.py
+++ app/api/property.py
@@ -13,6 +13,8 @@ from app.models import property as property_model
 from app.models import SessionLocal
 from app.schemas import property as property_schema
 
+from app.events.event_bus import event_bus
+
 router = APIRouter(prefix="/properties", tags=["properties"])
 
 # Dependency → DB Session bereitstellen
@@ -29,6 +31,7 @@ def create_property(property: property_schema.PropertyCreate, db: Session = Depe
     db_property = property_model.Property(**property.dict())
     db.add(db_property)
     db.commit()
+    event_bus.publish("PropertyCreated", db_property.id)
     db.refresh(db_property)
     return db_property
 
@@ -53,4 +56,5 @@ def delete_property(property_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Property not found")
     db.delete(db_property)
     db.commit()
+    event_bus.publish("PropertyDeleted", property_id)
     return {"ok": True}
